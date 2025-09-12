65a436f97c45183a005110e13f4583ff4e51728d
김주희 <juhui.925@gmail.com>
2025-09-12 13:25:02 +0900

    .

--- HC-SR04/Core/Src/main.c
+++ HC-SR04/Core/Src/main.c
@@ -8,7 +8,7 @@
   *
   * Copyright (c) 2025 STMicroelectronics.
   * All rights reserved.
-  * test
+  * test s
   * This software is licensed under terms that can be found in the LICENSE file
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
